2e1aeae78d4b84a062e281319a4f676dd62fe85b
MuradNazari <38508868+mur2d00@users.noreply.github.com>
2023-11-15 23:28:05 -0600

    Update writeData.py

--- writeData.py
+++ writeData.py
@@ -1,49 +1,52 @@
-import fluidfoam
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import os
-import timeit
+import sys
+import pandas as pd
+import vtk
+from vtk.util import numpy_support as VN
+import pyvista as pv
 
 
 
-os.system('cls' if os.name == 'nt' else 'clear')
-start = timeit.default_timer()
+#
 
-print("########## Reading Mesh ##########")
-x, y, z = fluidfoam.readof.readmesh('/work/nazari/2CylsedFoam/', structured=True, precision=12)
-nx, ny, nz = x.shape
+Directory = "/Users/murad/Library/CloudStorage/OneDrive-LouisianaStateUniversity/MLPractice/isosurfaces/"
+outputfile = Directory + "surface_1.vtp"
 
-print("########## Reading Concentration File ##########")
-from fluidfoam import readscalar
-sol = '/work/nazari/2CylsedFoam/'
-time = '2'
-alpha1 = fluidfoam.readscalar(sol, time, "alpha.a", True, precision=12)
-alpha_a = (np.mean(np.mean(alpha1, 0), 1))
+print ('Loading', outputfile)
 
-print("########## Reading Velocity File ##########")
-ua1 = fluidfoam.readvector(sol, time, "U.a", True, precision=12)
-ua_a = (np.mean(np.mean(ua1, 3), 1)) #Averaged by x and z  !!!!!
-ua_abox = np.zeros(ua1.shape)
-for j, xi in enumerate(x[:, 0, 0]):
-            for k, zi in enumerate(z[0, 0, :]):
-                ua_abox[:, j, :, k] = ua_a             
-# Calculating u'                
-ubprim1 = ua1 - ua_abox
+reader = vtk.vtkXMLPolyDataReader()
+reader.SetFileName(outputfile)
+reader.Update()
+data_vtk = reader.GetOutput()
+n_points = data_vtk.GetNumberOfPoints()
+print ('n_points of the mesh:' ,n_points)
+x_vtk = np.zeros((n_points,1))
+y_vtk = np.zeros((n_points,1))
+z_vtk = np.zeros((n_points,1))
+VTKpoints = vtk.vtkPoints()
 
-urms_a = np.sqrt(np.mean(np.mean((ubprim1[0, :, :, :] * ubprim1[0, :, :, :]), 2),0))
-vrms_a = np.sqrt(np.mean(np.mean((ubprim1[1, :, :, :] * ubprim1[1, :, :, :]), 2),0))
-wrms_a = np.sqrt(np.mean(np.mean((ubprim1[2, :, :, :] * ubprim1[2, :, :, :]), 2),0))
+for i in range(n_points):
+    pt_iso  =  data_vtk.GetPoint(i)
+    x_vtk[i] = pt_iso[0]   
+    y_vtk[i] = pt_iso[1]
+    z_vtk[i] = pt_iso[2]
+VTKpoints.InsertPoint(i, pt_iso[0], pt_iso[1], pt_iso[2])
+
+point_data = vtk.vtkUnstructuredGrid()
+point_data.SetPoints(VTKpoints)
+x  = np.reshape(x_vtk , (np.size(x_vtk [:]),1)) 
+y  = np.reshape(y_vtk , (np.size(y_vtk [:]),1))
+z  = np.reshape(z_vtk , (np.size(z_vtk [:]),1))
+
+#Plot the vtk data
+data = np.genfromtxt("/Users/murad/Library/CloudStorage/OneDrive-LouisianaStateUniversity/MLPractice/output_1.txt", delimiter= ' ');
+x_coord = data[:,0]
+y_coord = data[:,1]
+z_coord = data[:,2]
+grid = pv.read("/Users/murad/Library/CloudStorage/OneDrive-LouisianaStateUniversity/MLPractice/isosurfaces/surface_1.vtp")
+grid.plot(scalars='U.b', component=0, cmap='turbo', cpos='xy')
 
 
-print("########## Reading Bed Surface Elevation ##########")
-#isoalphaa file contains isosurfaces of the bed (alpha.a = 0.5) in x,y,z coordinate format
-bedelevation = pd.read_csv(r"/work/nazari/2CylsedFoam/postProcessing/surfaces/isoalphaa_1.raw", delimiter=' ')
-xDir = np.linspace (-1,1,100)
-yDir = np.linspace (-0.3,0.3,100)
-x,y = np.meshgrid(xDir,yDir)
-plt.contourf(x,y,bedelevation[:, 2])
-
-
-
-stop = timeit.default_timer()
-print('Time elapsed: ', round(stop - start), 'seconds')  
